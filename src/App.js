79649b0cb732231b8acfa13b9db27a55ba3b2a66
kenikall <kenikall@gmail.com>
2020-08-05 13:47:12 -0700

    moves port to App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import express from 'express';
 import ProbababilityPage from './components/ProbabilityPage'
 import './App.css';
 
 function App() {
+  const app = express();
+  app.set('port', process.env.PORT || 3000);
+  const port = app.get('port')
+
+  console.log(`Express server listening on port ${port}`);
+
+  app.listen(port);
+
   return (
     <ProbababilityPage />
   );
