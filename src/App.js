57ed9f85ac302e04c7fe03e0299d3c96190004b6
kenikall <kenikall@gmail.com>
2020-08-05 16:13:13 -0700

    reset app

--- src/App.js
+++ src/App.js
@@ -1,19 +1,8 @@
 import React from 'react';
-import express from 'express';
 import ProbababilityPage from './components/ProbabilityPage'
 import './App.css';
 
 function App() {
-  console.log('*****')
-  const app = express();
-  app.set('port', process.env.PORT || 3000);
-  const port = app.get('port')
-
-  console.log(`Express server listening on port ${port}`);
-  console.log('*****')
-
-  app.listen(port);
-
   return (
     <ProbababilityPage />
   );
